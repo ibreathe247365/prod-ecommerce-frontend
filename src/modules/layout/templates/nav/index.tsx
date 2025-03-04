2f899a2da088b79752bb6ba86221ec942c4ab134
ibreathe247365 <76894659+ibreathe247365@users.noreply.github.com>
2025-03-04 10:24:40 +0000

    oval bigger

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -12,7 +12,7 @@ export default async function Nav() {
 
   return (
     <div className="sticky top-0 inset-x-0 z-50 group">
-      <header className="relative h-20 mx-auto border-b duration-200 bg-navbar-gradient border-ui-fg-base">
+      <header className="relative h-20 mx-auto  duration-200 bg-navbar-gradient border-ui-fg-base">
         <nav className="content-container txt-xsmall-plus text-ui-fg-base flex items-center justify-between w-full h-full text-small-regular relative">
           <div className="flex-1 basis-0 h-full flex items-center">
             <div className="h-full">
@@ -21,13 +21,13 @@ export default async function Nav() {
           </div>
 
           {/* White oval with Babies text */}
-          <div className="absolute left-1/2 transform -translate-x-1/2 bg-white px-6 py-2 rounded-full shadow-md">
+          <div className="absolute left-1/2 transform -translate-x-1/2 bg-white px-10 py-4 rounded-full shadow-lg">
             <LocalizedClientLink
               href="/"
-              className="txt-compact-xlarge-plus text-blue-800 hover:text-blue-900 uppercase"
+              className="txt-compact-xlarge-plus text-blue-800 hover:text-blue-900"
               data-testid="nav-store-link"
             >
-              Babies
+              babies
             </LocalizedClientLink>
           </div>
 
